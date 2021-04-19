88749bf213310fe3470e048c1a8fac3fdc9d01a4
Allen Liu <kamyu104@gmail.com>
2021-04-19 14:15:15 +0800

    update

--- Round B/longest_progression2.py
+++ Round B/longest_progression2.py
@@ -15,25 +15,17 @@ def longest_progression():
     dp_skip = [0]*4
     result = 0
     for i in xrange(N):
-        dp[i%4] = min(i+1, 2)
-        dp_skip[i%4] = min(i+1, 2+1)
-        result = max(result, min(dp[i%4]+1, N), dp_skip[i%4])
-        if i < 2:
-            continue
-        if A[i]-A[i-1] == A[i-1]-A[i-2]:  # no replace
+        dp[i%4], dp_skip[i%4] = min(i+1, 2), min(i+1, 2+1)
+        if i >= 2 and A[i]-A[i-1] == A[i-1]-A[i-2]:  # no replace
             dp[i%4] = dp[(i-1)%4]+1
             dp_skip[i%4] = dp_skip[(i-1)%4]+1
-            result = max(result, min(dp[i%4]+1, N), dp_skip[i%4])
-        if i < 3:
-            continue
-        if A[i]-A[i-2] == 2*(A[i-2]-A[i-3]):  # replace A[i-1]
+        if i >= 3 and A[i]-A[i-2] == 2*(A[i-2]-A[i-3]):  # replace A[i-1]
             result = max(result, dp[(i-2)%4]+1+1)
-        if 2*(A[i]-A[i-1]) == A[i-1]-A[i-3]:  # replace A[i-2]
+        if i >= 3 and 2*(A[i]-A[i-1]) == A[i-1]-A[i-3]:  # replace A[i-2]
             dp_skip[i%4] = max(dp_skip[i%4], 4)
-            result = max(result, dp_skip[i%4])
             if i >= 4 and A[i]-A[i-1] == A[i-3]-A[i-4]:
                 dp_skip[i%4] = max(dp_skip[i%4], dp[(i-3)%4]+1+2)
-                result = max(result, dp_skip[i%4])
+        result = max(result, min(dp[i%4]+1, N), dp_skip[i%4])
     return result
 
 for case in xrange(input()):
