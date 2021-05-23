4156626956e1b6291bd5e192096d821eb7e003c5
Allen Liu <kamyu104@gmail.com>
2021-05-23 23:27:23 +0800

    update

--- Round C/smaller_strings.py
+++ Round C/smaller_strings.py
@@ -11,15 +11,16 @@ def smaller_strings():
     N, K = map(int, raw_input().strip().split())
     S = raw_input().strip()
 
-    result = 0
-    cnt = pow(K, (len(S)+1)//2-1, MOD)  # Time: O(logN)
-    inv_K = pow(K, MOD-2, MOD)  # Time: O(logN), Fermat's little theorem, k^(p-1) % p = 1 => k^(p-2) % p = k^(-1) % p
-    for i in xrange((len(S)+1)//2):
+    result, cnt = 0, 1
+    has_diff = False
+    for i in reversed(xrange((len(S)+1)//2)):
         result = (result+(ord(S[i])-ord('a'))*cnt)%MOD
-        cnt = (cnt*inv_K)%MOD
-    i = next((i for i in reversed(xrange(len(S)//2)) if S[i] != S[-1-i]), -1)
-    if i != -1:
-        result = (result+int(S[i] < S[-1-i]))%MOD
+        cnt = (cnt*K)%MOD
+        if has_diff:
+            continue
+        if S[i] != S[-1-i]:
+            has_diff = True
+            result = (result+int(S[i] < S[-1-i]))%MOD
     return result
 
 MOD =10**9+7
