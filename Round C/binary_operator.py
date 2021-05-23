f7266b13872dfc402763a18dd8e4045a5b387c2e
Allen Liu <kamyu104@gmail.com>
2021-05-24 04:59:20 +0800

    update

--- Round C/binary_operator.py
+++ Round C/binary_operator.py
@@ -9,7 +9,7 @@
 
 from random import seed, randint
 
-def binary_operator():
+def solution(N, E):
     lookup = {}
     class Operand(object):
         def __init__(self, state=None):
@@ -20,18 +20,28 @@ def binary_operator():
 
         def __add__(self, other):
             if (self.state, other) not in lookup:
-                lookup[(self.state, other)] = randint(10**9, 10**18)
+                lookup[(self.state, other)] = randint(1, MAX_N*MAX_E)
             return lookup[(self.state, other)]
 
-    N = input()
     result, groups, x = [], {}, Operand()
-    for _ in xrange(N):
-        g = eval(raw_input().strip().replace("#", "+x+"))
+    for e in E:
+        g = eval(e.replace("#", "+x+"))
         if g not in groups:
             groups[g] = len(groups)+1
         result.append(str(groups[g]))
     return " ".join(result)
 
+def binary_operator():
+    N = input()
+    E = [raw_input().strip() for _ in xrange(N)]
+    prev = None
+    while True:
+        curr = solution(N, E)
+        if curr == prev:
+            return curr
+        prev = curr
+
 seed(0)
+MAX_N = MAX_E = 100
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, binary_operator())
