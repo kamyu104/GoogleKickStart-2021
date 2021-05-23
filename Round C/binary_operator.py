6fcf7da25145b695610bdbc64584983d2bc50712
Allen Liu <kamyu104@gmail.com>
2021-05-24 02:58:44 +0800

    update

---
+++ Round C/binary_operator.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2021 kamyu. All rights reserved.
+#
+# Google Kick Start 2021 Round C - Problem D. Binary Operator
+# https://codingcompetitions.withgoogle.com/kickstart/round/0000000000435c44/00000000007ec290
+#
+# Time:  O(N * E)
+# Space: O(N + E)
+#
+
+from random import randint, seed
+
+def binary_operator():
+    lookup = {}
+    class Operand(object):
+        def __init__(self, state=None):
+            self.state = state
+
+        def __radd__(self, other):
+            return Operand(other)
+
+        def __add__(self, other):
+            if (self.state, other) not in lookup:
+                lookup[(self.state, other)] = randint(10**9, 10**18)
+            return lookup[(self.state, other)]
+
+    N = input()
+    result, group, x = [], {}, Operand()
+    for _ in xrange(N):
+        g = eval(raw_input().strip().replace("#", "+x+"))
+        if g not in group:
+            group[g] = len(group)+1
+        result.append(str(group[g]))
+    return " ".join(result)
+
+seed(0)
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, binary_operator())
