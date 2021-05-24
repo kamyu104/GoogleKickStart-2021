700a752857a8d68ccba07105cc9d8cc7effbe7ff
Allen Liu <kamyu104@gmail.com>
2021-05-24 10:36:23 +0800

    update

--- Round C/binary_operator2.py
+++ Round C/binary_operator2.py
@@ -3,44 +3,32 @@
 # Google Kick Start 2021 Round C - Problem D. Binary Operator
 # https://codingcompetitions.withgoogle.com/kickstart/round/0000000000435c44/00000000007ec290
 #
-# Time:  O(N * E)
+# Time:  O(N * E^2)
 # Space: O(N + E)
 #
 
 from random import seed, randint
-from functools import partial
-
-def addmod(x, y):
-    return (x+y)%MOD
-
-def mulmod(x, y):
-    return (x*y)%MOD
 
 def hash(lookup, x, y):
     if (x, y) not in lookup:
         lookup[(x, y)] = randint(0, MOD-1)
     return lookup[(x, y)]    
 
-def evaluate(s, ops):
-    operands, operators, operand = [], [], 0
-    for i in xrange(len(s)):
-        if s[i].isdigit():
-            operand = addmod(operand*10, int(s[i]))
-            if i == len(s)-1 or not s[i+1].isdigit():
-                operands.append(operand)
-                operand = 0
-        elif s[i] == ')':
-            right, left = operands.pop(), operands.pop()
-            operands.append(ops[operators.pop()](left, right))
-        elif s[i] != '(':
-            operators.append(s[i])
-    return operands[-1]
-
 def count(E):
-    result, groups = [], {}
-    ops = {'+':addmod, '*':mulmod, '#':partial(hash, {})}
-    for s in E:
-        g = evaluate(s, ops)
+    lookup = {}
+    class Operand(object):
+        def __init__(self, state=None):
+            self.state = state
+
+        def __radd__(self, other):
+            return Operand(other)
+
+        def __add__(self, other):
+            return hash(lookup, self.state, other)
+
+    result, groups, x = [], {}, Operand()
+    for e in E:
+        g = eval(e.replace("#", "+x+"))
         if g not in groups:
             groups[g] = len(groups)+1
         result.append(str(groups[g]))
@@ -61,7 +49,7 @@ def binary_operator():
             return curr
 
 seed(0)
-MOD = 10**9-63  # a prime rather than 10**9+7 to avoid collision made by corner case
+MOD = 10**9+7
 K = 2
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, binary_operator())
