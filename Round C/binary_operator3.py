08d66277ac0997e1b381581c679a0083e199889f
Allen Liu <kamyu104@gmail.com>
2021-05-24 16:27:01 +0800

    update

--- Round C/binary_operator3.py
+++ Round C/binary_operator3.py
@@ -61,7 +61,7 @@ def binary_operator():
             return curr
 
 seed(0)
-MOD = 10**9-63  # a prime rather than 10**9+7 to avoid collision made by corner case
+MOD = 10**9-63  # pick a prime rather than 10**9+7 to avoid collision made by corner case
 K = 2
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, binary_operator())
