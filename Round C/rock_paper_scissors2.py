5a51d5139af0c9a912f563dab12919b19f0c36c3
Allen Liu <kamyu104@gmail.com>
2021-05-24 20:42:28 +0800

    update

--- Round C/rock_paper_scissors2.py
+++ Round C/rock_paper_scissors2.py
@@ -20,38 +20,34 @@ def rock_paper_scissors():
 
 '''
 N = 60
-def backtracing(W, E, dp, r, s, p):
+def backtracing(W, E, dp):
     result = []
-    while r+s+p:
-        if r-1 >= 0 and dp[r][s][p] == (dp[r-1][s][p] + (W*p+E*s)/((r-1)+p+s) if (r-1)+s+p != 0 else (W+E)/3):
-            result.append('R')
-            r -= 1
-        elif s-1 >= 0 and dp[r][s][p] == (dp[r][s-1][p] + (W*r+E*p)/(r+(s-1)+p) if r+(s-1)+p != 0 else (W+E)/3):
-            result.append('S')
-            s -= 1
+    r = s = p = 0
+    while r+s+p < N:
+        result.append(dp[r][s][p][1])
+        if result[-1] == 'R':
+            r += 1
+        elif result[-1] == 'S':
+            s += 1
         else:
-            result.append('P')
-            p -= 1
-    result.reverse()
+            p += 1
     return "".join(result)
 
 def solve(W, E):
-    dp = [[[0.0]*(N+1) for _ in xrange(N+1)] for _ in xrange(N+1)]
-    max_r = max_s = -1
-    for r in xrange(N+1):
-        for s in xrange(N+1-r):
-            for p in xrange(N+1-r-s-1):  # skip r+s+p = N
-                dp[r+1][s][p] = max(dp[r+1][s][p], dp[r][s][p] + (W*p+E*s)/(r+s+p) if r+s+p != 0 else (W+E)/3)
-                dp[r][s+1][p] = max(dp[r][s+1][p], dp[r][s][p] + (W*r+E*p)/(r+s+p) if r+s+p != 0 else (W+E)/3)
-                dp[r][s][p+1] = max(dp[r][s][p+1], dp[r][s][p] + (W*s+E*r)/(r+s+p) if r+s+p != 0 else (W+E)/3)
-            if max_r == -1 or dp[max_r][max_s][N-max_r-max_s] < dp[r][s][N-r-s]:
-                max_r, max_s = r, s
-    return backtracing(W, E, dp, max_r, max_s, N-max_r-max_s)
+    dp = [[[[-1.0, ''] for _ in xrange(N+1)] for _ in xrange(N+1)] for _ in xrange(N+1)]
+    for r in reversed(xrange(N+1)):
+        for s in reversed(xrange(N+1-r)):
+            dp[r][s][N-r-s] = [0.0, '']
+            for p in reversed(xrange(N+1-r-s-1)):
+                dp[r][s][p] = max(dp[r][s][p], [dp[r+1][s][p][0] + ((W*p+E*s)/(r+s+p) if r+s+p != 0 else (W+E)/3), 'R'])
+                dp[r][s][p] = max(dp[r][s][p], [dp[r][s+1][p][0] + ((W*r+E*p)/(r+s+p) if r+s+p != 0 else (W+E)/3), 'S'])
+                dp[r][s][p] = max(dp[r][s][p], [dp[r][s][p+1][0] + ((W*s+E*r)/(r+s+p) if r+s+p != 0 else (W+E)/3), 'P'])
+    return backtracing(W, E, dp)
 
 # precompute
 RESULT = [solve(1.0, 1.0/x) for x in [1, 2, 10, float("inf")]]
 '''
-RESULT = ['PSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSR', 'PRRSSSSPPPPPPPPRRRRRRRRRRRRRRRRSSSSSSSSSSSSSSSSSSSPPPPPPPPPP', 'SPPPPRRRRRRRRRRRRRRRSSSSSSSSSSSSSSSSSSSSSSSSSPPPPPPPPPPPPPPP', 'PRRRRRRRRRSSSSSSSSSSSSSSSSSSSSSSSSSSSPPPPPPPPPPPPPPPPPPPPPPP']
+RESULT = ['SRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRPSRP', 'SPPRRRRSSSSSSSSPPPPPPPPPPPPPPPPRRRRRRRRRRRRRRRRRRRSSSSSSSSSS', 'SPPPPRRRRRRRRRRRRRRRSSSSSSSSSSSSSSSSSSSSSSSSSPPPPPPPPPPPPPPP', 'SPPPPPPPPPRRRRRRRRRRRRRRRRRRRRRRRRRRRSSSSSSSSSSSSSSSSSSSSSSS']
 T, X = input(), input()
 for case in xrange(T):
     print 'Case #%d: %s' % (case+1, rock_paper_scissors())
