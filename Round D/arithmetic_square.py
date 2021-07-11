9b69f8ae15e86513247b6a8b8e6370ec8c55ed36
Allen Liu <kamyu104@gmail.com>
2021-07-12 05:22:28 +0800

    update

--- Round D/arithmetic_square.py
+++ Round D/arithmetic_square.py
@@ -20,7 +20,7 @@ def arithmetic_square():
     result = sum(G[i]+G[j] == 2*G[k] for i, j, k in [(0, 2, 1), (5, 7, 6), (0, 5, 3), (2, 7, 4)])
     count = Counter()
     for i, j in [(0, 7), (2, 5), (1, 6), (3, 4)]:
-        if (G[i]+G[j]) % 2 == 0:
+        if (G[i]+G[j])%2 == 0:
             count[G[i]+G[j]] += 1
     return result + max(count.values() or [0])
 
