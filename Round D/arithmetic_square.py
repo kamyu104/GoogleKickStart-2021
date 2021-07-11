3f97b8f4d4b66e99409951b29514b3112bf882dd
Allen Liu <kamyu104@gmail.com>
2021-07-11 17:49:01 +0800

    update

--- Round D/arithmetic_square.py
+++ Round D/arithmetic_square.py
@@ -13,9 +13,10 @@ def arithmetic_square():
     G = []
     for _ in xrange(3):
         G.extend(map(int, raw_input().strip().split()))
-    # 0 1 2
-    # 3   4
-    # 5 6 7
+    # layout of G:
+    #   0 1 2
+    #   3   4
+    #   5 6 7
     result = sum(G[i]+G[j] == 2*G[k] for i, j, k in [(0, 2, 1), (5, 7, 6), (0, 5, 3), (2, 7, 4)])
     count = Counter()
     for i, j in [(0, 7), (2, 5), (1, 6), (3, 4)]:
