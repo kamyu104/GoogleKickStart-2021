29aece10bdd2af69fdaad56325ad17eccea9fb29
Allen Liu <kamyu104@gmail.com>
2021-08-23 04:38:29 +0800

    update

--- Round E/birthday_cake.py
+++ Round E/birthday_cake.py
@@ -20,7 +20,7 @@ def birthday_cake():
               min(2*ceil_divide(m, K)+ceil_divide(n, K)+ceil_divide(min(r2, R-(r1-1)), K),
                   2*ceil_divide(n, K)+ceil_divide(m, K)+ceil_divide(min(c2, C-(c1-1)), K))
     full = (n*m-1) + (ceil_divide(n, K)-1)*(ceil_divide(m, K)-1)
-    return partial+full  # cut of partially delicious cake and cut of fully delicious cake
+    return partial+full  # cuts of partially delicious cake and cuts of fully delicious cake
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, birthday_cake())
