a04eaad530198248d618fb0fc6b99b40ae9431b0
Allen Liu <kamyu104@gmail.com>
2021-09-19 06:04:30 +0800

    update

--- Round F/star_trappers.py
+++ Round F/star_trappers.py
@@ -7,8 +7,8 @@
 # Space: O(1)
 #
 
-def ccw(A, B, C):
-    return (B[0]-A[0])*(C[1]-A[1]) - (B[1]-A[1])*(C[0]-A[0])
+def ccw(a, b, c):
+    return (b[0]-a[0])*(c[1]-a[1]) - (b[1]-a[1])*(c[0]-a[0])
 
 def vector(a, b):
     return [a[i]-b[i] for i in xrange(len(a))]
@@ -22,8 +22,8 @@ def inner_product(a, b):
 def is_between(t, a, b):
     return ccw(t, a, b) == 0 and 0 < inner_product(vector(a, t), vector(a, b)) < inner_product(vector(a, b), vector(a, b))
 
-def is_inside_triangle(T, A, B, C):
-    d1, d2, d3 = ccw(T, A, B),  ccw(T, B, C),  ccw(T, C, A)
+def is_inside_triangle(t, a, b, c):
+    d1, d2, d3 = ccw(t, a, b),  ccw(t, b, c),  ccw(t, c, a)
     return (d1 > 0 and d2 > 0 and d3 > 0) or (d1 < 0 and d2 < 0 and d3 < 0)
 
 def star_trappers():
