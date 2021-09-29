8d1f7b0bbc0995abf55627dc9f7872fc8c37ab42
Allen Liu <kamyu104@gmail.com>
2021-09-29 14:08:21 +0800

    update

--- Round F/festival4.py
+++ Round F/festival4.py
@@ -8,6 +8,7 @@
 #
 
 from heapq import heappush, heappop
+from collections import defaultdict
 
 def festival():
     D, N, K = map(int, raw_input().strip().split())
@@ -18,12 +19,10 @@ def festival():
         points.append((e+1, -1, h))
     points.sort()
 
-    topk, others, topk_to_remove, others_to_remove = [], [], [], []
+    topk, others, to_remove = [], [], defaultdict(int)
     result = curr = l = 0
     for _, c, h in points:
         if c == 1:
-            while topk and topk_to_remove and topk[0] == topk_to_remove[0]:
-                heappop(topk), heappop(topk_to_remove)
             heappush(topk, h)
             curr += h
             l += 1
@@ -34,20 +33,25 @@ def festival():
                 heappush(others, -v)
             result = max(result, curr)
         else:
-            while others and others_to_remove and others[0] == others_to_remove[0]:
-                heappop(others), heappop(others_to_remove)
-            if others and h <= -others[0]:
-                heappush(others_to_remove, -h)
-                continue
-            heappush(topk_to_remove, h)
-            curr -= h
-            l -= 1
-            if not others:
-                continue
-            v = -heappop(others)
-            heappush(topk, v)  # keep topk with k elements
-            curr += v
-            l += 1
+            to_remove[h] += 1
+            if not others or -others[0] < h:
+                curr -= h
+                l -= 1
+                if others:
+                    v = -heappop(others)
+                    heappush(topk, v)  # keep topk with k elements
+                    curr += v
+                    l += 1
+        while others and -others[0] in to_remove:
+            to_remove[-others[0]] -= 1
+            if not to_remove[-others[0]]:
+                del to_remove[-others[0]]
+            heappop(others)
+        while topk and topk[0] in to_remove:
+            to_remove[topk[0]] -= 1
+            if not to_remove[topk[0]]:
+                del to_remove[topk[0]]
+            heappop(topk)
     return result
 
 for case in xrange(input()):
