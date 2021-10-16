174379d41107d6e8bd670b8e6bac5dc139c64f1c
Allen Liu <kamyu104@gmail.com>
2021-10-17 07:01:06 +0800

    update

--- Round G/simple_polygon.py
+++ Round G/simple_polygon.py
@@ -12,19 +12,18 @@ def simple_polygon():
 
     if N-A > 2:
         return "IMPOSSIBLE"
-    if N == 3:
-        result = [(0, A), (1, 0), (0, 0)]
-    else:
-        assert(A-N+4 <= MAX_A)
-        # if A-N+4 > MAX_A,
-        # => 3 <= N < A-MAX_A+4 <= 4,
-        # => N = 3
-        # => -><-
-        result = [(0, A-N+4)]
+    if A != MAX_A or N != 3:
+        # the last one and the first two points forms 1 unit, the rest of points increases 1 unit,
+        # and the first point is (0, 2+dy), where dy = (A-1)-(N-3)
+        # => 2+dy = 2+((A-1)-(N-3)) = A-N+4 <= MAX_A iff A != MAX_A or N != 3
+        assert(2+((A-1)-(N-3)) <= MAX_A)
+        result = [(0, 2+((A-1)-(N-3)))]
         for i in xrange(1, N//2):
             result.append((i, 1 if i%2 else 2))
         for i in reversed(xrange((N+1)//2)):
             result.append((i, 1 if not i%2 or (i == (N+1)//2-1 and N%4 == 3) else 0))
+    else:
+        result = [(0, A), (1, 0), (0, 0)]
     return "POSSIBLE\n" + "\n".join(map(lambda x: "%s %s"%(x[0], x[1]), result))
 
 MAX_A = 10**9
