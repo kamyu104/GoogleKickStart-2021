2ae153cb475dbd6e941fe6e8e45eac341045f341
Allen Liu <kamyu104@gmail.com>
2021-11-15 08:33:15 +0800

    update

--- Round H/transform_the_string.py
+++ Round H/transform_the_string.py
@@ -8,9 +8,8 @@
 #
 
 def transform_the_string():
-    S, F = [raw_input().strip() for _ in xrange(2)]
-
-    dist = {c:min(min(abs((ord(f)-ord('a'))-(ord(c)-ord('a'))), 26-abs((ord(f)-ord('a'))-(ord(c)-ord('a')))) for f in F) for c in set(list(S))}
+    S, F = [map(lambda x: ord(x)-ord('a'), list(raw_input().strip())) for _ in xrange(2)]
+    dist = {c:min(min(abs(f-c), 26-abs(f-c)) for f in F) for c in set(S)}
     return sum(dist[c] for c in S)
 
 for case in xrange(input()):
